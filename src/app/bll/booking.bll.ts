b38e9d6ecd80c2a2b419124a31e427b76ad647b6
abhinash1998 <abhinashb@mindfiresolutions.com>
2022-08-01 09:55:20 +0530

    Added getLatestBookingByUserId api end point

--- src/app/bll/booking.bll.ts
+++ src/app/bll/booking.bll.ts
@@ -1,10 +1,13 @@
 import errorLogBLL from "../bll/error-log.bll";
 import Booking from "../models/booking.model";
+import Movie from "../models/movie.model";
+import Show from "../models/show.model";
+import Theatre from "../models/theatre.model";
 
 export default class bookingBLL {
     async createBooking(bookingObject) {
         try {
-            const { status, numberOfSeats, seats, amount, customerId, theatreName, movieName } = bookingObject;
+            const { status,numberOfSeats,seats,amount,customerId,theatreName,movieName } = bookingObject;
 
             const booking = new Booking({
                 status,
@@ -33,7 +36,7 @@ export default class bookingBLL {
 
     async getBookings() {
         try {
-            const result = await Booking.find().populate("customerId");
+            const result = await Booking.find().populate("customerId"); 
 
             return {
                 status: true,
@@ -48,10 +51,29 @@ export default class bookingBLL {
         }
     }
 
+
     async getBookingByUserId(bookingObject) {
         try {
 
-            const result = await Booking.find({ customerId: bookingObject.userId });
+            const result = await Booking.find({customerId: bookingObject.userId});
+
+            return {
+                status: true,
+                result: result
+            };
+        } catch (error) {
+            await new errorLogBLL().logError('bookingBLL', 'getBookingByUserId', error);
+            return {
+                status: false,
+                error: error.message
+            }
+        }
+    }
+
+    async getLatestBookingByUserId(bookingObject) {
+        try {
+
+            const result = await Booking.findOne({customerId: bookingObject.userId}).populate("customerId").sort({ _id: -1 })
 
             return {
                 status: true,
