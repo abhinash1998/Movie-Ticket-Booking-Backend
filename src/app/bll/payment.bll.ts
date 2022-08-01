bf507a57c083dcce778dfa43ce3f54dbed905ffb
abhinash1998 <abhinashb@mindfiresolutions.com>
2022-08-01 09:53:18 +0530

    Payment using Stripe Payment Gateway

--- src/app/bll/payment.bll.ts
+++ src/app/bll/payment.bll.ts
@@ -1,24 +1,34 @@
 import errorLogBLL from "../bll/error-log.bll";
-import Booking from "../models/booking.model";
-import Payment from "../models/payment.model";
+import Stripe from "stripe";
+
+const secret = process.env.STRIPE_SECRET;
+ const stripe = new Stripe('sk_test_51LQVt5SIWQnYJ3odwy3k1glEG8DB5nUK9vpNGVjP7tKeZtatGzOpX9v6n5oWHjUJK6qC98JHaLG8EmOVBJuHovfw00uMtKe4Pz', {
+    apiVersion: '2020-08-27',
+  });
 
 export default class paymentBLL {
     async createPayment(paymentObject) {
         try {
-            const { amount,bookingId } = paymentObject;
+            console.log(paymentObject)
 
-            const payment = new Payment({
-                amount,
-                bookingId,
-                timeStamp: new Date()
+            const customer =   await stripe.customers
+            .create({
+              email: paymentObject.email,
+            });
+            
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount: paymentObject.amount * 100,
+              description: "Cinema Hall Ticket Booking",
+              currency: "INR"
             });
 
-            const result = await payment.save();
-            return {
-                status: true,
-                result: result
-            };
-        } catch (error) {
+                return {
+                    status: true,
+                    result: paymentIntent
+                };
+          } 
+         
+         catch (error) {
             await new errorLogBLL().logError('paymentBLL', 'createPayment', error);
             return {
                 status: false,
@@ -26,22 +36,4 @@ export default class paymentBLL {
             }
         }
     }
-
-    async getPaymentById(paymentObject) {
-        try {
-
-            const result = await Payment.findById(paymentObject.paymentId);
-
-            return {
-                status: true,
-                result: result
-            };
-        } catch (error) {
-            await new errorLogBLL().logError('paymentBLL', 'getPaymentById', error);
-            return {
-                status: false,
-                error: error.message
-            }
-        }
-    }
-}   
+}  
--- src/app/controllers/payment.controller.ts
+++ src/app/controllers/payment.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import bookingBLL from "../bll/booking.bll";
 import paymentBLL from "../bll/payment.bll";
 import eErrorMessage from "../enum/error-message.enum";
 
@@ -23,21 +22,3 @@ export const createPayment = async (req: Request, res: Response) => {
     }
 };
 
-export const getPaymentById = async (req: Request, res: Response) => {
-    try {
-        if (!Object.keys(req.query).length || !req.query.paymentId) {
-            return res.status(StatusCodes.BAD_REQUEST).send({
-                message: eErrorMessage.FieldContent
-            });
-        }
-        const result = await new paymentBLL().getPaymentById(req.query);
-        if (result.status) {
-            return res.status(StatusCodes.OK).send(result);
-        }
-        else {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(result);
-        }
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
-    }
-};
